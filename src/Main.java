289ba3df6e30a43142372fcd585913b8b849ce2f
merQ224 <wnwinstonn95@gmail.com>
2026-05-16 19:00:51 +1200

    chore: moved files into src folder

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+package src;
 import java.util.logging.Logger;
 
 public class Main {
+    // static: shared by the class
     private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    // main() needs to run before any objects exist
     public static void main(String[] args) {
         Book book = new Book(
             "Hunger Games", 
